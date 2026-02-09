27a63923a63764c3bff56fdd8f1502e10b63e6f6
Robin Ryan <robin1ryan@hotmail.com>
2026-02-09 11:16:07 +0200

    Abstracted chart & fixed some bugs. Now the code is cleaner, and it can scale and updates correctly if width or height is changed

--- src/components/binanceLiquiMap/components/timeLapsChart.tsx
+++ src/components/binanceLiquiMap/components/timeLapsChart.tsx
@@ -10,47 +10,12 @@ import { useEffect, useRef } from "react";
  * Zooming HeatMap also highlights the min/max time currently showed on Heatmap
  */
 
-const TimeLapsChart = ({ data }) => {
-  const svgRef = useRef(null);
-
-  const margins = { top: 70, right: 60, bottom: 50, left: 80 };
-  const height = 500;
-  const width = 1100;
-  const innerWidth = width - margins.left - margins.right;
-  const innerHeight = height - margins.top - margins.bottom;
-
-  const dateParser = d3.timeParse("%Y %m %d");
-
+const useChart = (gRef, data, innerHeight, x, y, margins) => {
   useEffect(() => {
-    if (!svgRef.current || !data) return;
-    const svgElement = d3.select(svgRef.current);
-    svgElement.selectAll("*").remove();
+    if (!gRef.current || !data) return;
+    const g = d3.select(gRef.current);
 
-    const x = d3
-      .scaleTime()
-      .range([0, innerWidth])
-      .domain(d3.extent(data, (d) => new Date(d.date)));
-
-    const y = d3
-      .scaleLinear()
-      .range([innerHeight, 0])
-      .domain([0, d3.max(data, (d) => d.value)]);
-
-    const g = svgElement
-      .append("g")
-      .attr("transform", `translate(${margins.left},${margins.top})`);
-
-    console.log(data);
-
-    g.append("g")
-      .attr("transform", `translate(0, ${innerHeight})`)
-      .call(d3.axisBottom(x));
-
-    g.append("g")
-      .attr("transform", `translate(${innerWidth},0)`)
-      .call(
-        d3.axisRight(y).tickFormat((d) => (isNaN(d) ? "" : `${d.toFixed(2)}`)),
-      );
+    g.selectAll("*").remove();
 
     const line = d3
       .line()
@@ -77,28 +42,79 @@ const TimeLapsChart = ({ data }) => {
       .attr("stroke", "#002570")
       .attr("stroke-width", 1)
       .attr("d", line);
+  }, [data, gRef, margins]);
+};
 
-    const dragAreaStart = data.slice(50);
+const TimeLapsChart = ({ data }) => {
+  const svgRef = useRef(null);
+  const gRefStale = useRef(null);
+  const gRefMoveable = useRef(null);
 
-    console.log(dragAreaStart);
+  const margins = { top: 70, right: 60, bottom: 50, left: 80 };
+  const height = 250;
+  const width = 1100;
+  const innerWidth = width - margins.left - margins.right;
+  const innerHeight = height - margins.top - margins.bottom;
 
-    g.append("path")
-      .datum(dragAreaStart)
-      .attr("class", "area")
-      .attr("d", area)
-      .style("fill", "#003eb9")
-      .style("opacity", 0.5);
+  const x = d3
+    .scaleTime()
+    .range([0, innerWidth])
+    .domain(d3.extent(data, (d) => new Date(d.date)));
 
-    g.append("path")
-      .datum(dragAreaStart)
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", "#003eb9")
-      .attr("stroke-width", 1)
-      .attr("d", line);
-  }, [data]);
+  const y = d3
+    .scaleLinear()
+    .range([innerHeight, 0])
+    .domain([0, d3.max(data, (d) => d.value)]);
 
-  return <svg id="svgRef" ref={svgRef} style={{ width, height }} />;
+  useEffect(() => {
+    if (!svgRef.current || !data) return;
+
+    const svgRefElement = d3.select(svgRef.current);
+    svgRefElement.selectAll(".axis").remove();
+
+    svgRefElement
+      .append("g")
+      .attr("class", "axis")
+      .attr("transform", `translate(0, ${innerHeight})`);
+
+    svgRefElement
+      .append("g")
+      .attr("class", "axis")
+      .attr("transform", `translate(${innerWidth},0)`);
+  }, [data, svgRef, margins, gRefStale, gRefMoveable]);
+
+  useChart(gRefStale, data, innerHeight, x, y, margins);
+
+  // Data that will adjust the width of top-chart
+  const sliceMin = data.length - 16,
+    sliceMax = 16;
+
+  const slicedData = data.slice(sliceMax, sliceMin);
+
+  useChart(gRefMoveable, slicedData, innerHeight, x, y, margins);
+
+  return (
+    <>
+      <svg
+        id="svgRef"
+        ref={svgRef}
+        style={{
+          width,
+          height,
+          transform: `translate(${margins.left}, ${margins.top})`,
+        }}
+      >
+        <g
+          ref={gRefStale}
+          style={{ transform: `translate(${(margins.left, margins.top)})` }}
+        />
+        <g
+          ref={gRefMoveable}
+          style={{ transform: `translate(${(margins.left, margins.top)})` }}
+        />
+      </svg>
+    </>
+  );
 };
 
 export default TimeLapsChart;
