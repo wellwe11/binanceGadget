231fbe9f0b7b70ad1cc707012998cf7fcb2e7784
Robin Ryan <robin1ryan@hotmail.com>
2026-02-13 08:57:18 +0200

    style(inputRange): Updated style for thumbs

--- src/components/binanceLiquiMap/components/timeLapsChart/components/inputRange.tsx
+++ src/components/binanceLiquiMap/components/timeLapsChart/components/inputRange.tsx
@@ -18,21 +18,28 @@ const InputRange = ({ val, setter, max = 100 }: InputRangeInterface) => {
 
           [&::-webkit-slider-thumb]:appearance-none
           [&::-webkit-slider-thumb]:pointer-events-auto
-          [&::-webkit-slider-thumb]:w-3
-          [&::-webkit-slider-thumb]:h-3
+          [&::-webkit-slider-thumb]:w-4
+          [&::-webkit-slider-thumb]:h-4
           [&::-webkit-slider-thumb]:rounded-full
           [&::-webkit-slider-thumb]:bg-[#0000007c]
           [&::-webkit-slider-thumb]:border
           [&::-webkit-slider-thumb]:border-[#9c9c9c]
 
+          hover:[&::-webkit-slider-thumb]:border-[#dbdbdb]
+          hover:[&::-webkit-slider-thumb]:bg-[#4b4b4b]
+
           [&::-moz-range-thumb]:pointer-events-auto
-          [&::-moz-range-thumb]:w-3
-          [&::-moz-range-thumb]:h-3
+          [&::-moz-range-thumb]:w-4
+          [&::-moz-range-thumb]:h-4
           [&::-moz-range-thumb]:rounded-full
           [&::-moz-range-thumb]:bg-[#0000007c]
           [&::-moz-range-thumb]:border
           [&::-moz-range-thumb]:border-[#a4a4a4]
-          [&::-moz-range-thumb]:border-solid"
+          [&::-moz-range-thumb]:border-solid
+          
+          hover:[&::-moz-range-thumb]:border-[#dbdbdb]
+          hover:[&::-moz-range-thumb]:bg-[#4b4b4b]
+          "
       type="range"
       min="1"
       max={max - 1}
