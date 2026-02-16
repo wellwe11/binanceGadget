cd8587c51157bfc83f62a600ed8c534a2018ebf8
Robin Ryan <robin1ryan@hotmail.com>
2026-02-16 12:45:14 +0200

    moved Axis into it's own file

--- src/components/binanceLiquiMap/components/liquidationMap/liquidationMap.tsx
+++ src/components/binanceLiquiMap/components/liquidationMap/liquidationMap.tsx
@@ -1,56 +1,7 @@
 import * as d3 from "d3";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import filterByType from "./functions/filterByType";
-
-const Axis = ({ children, height, width, margin, x, xBars, y }) => {
-  const svgRef = useRef(null);
-  const xAxisRef = useRef(null);
-  const xBarAxisRef = useRef(null);
-  const yAxisRef = useRef(null);
-
-  const xAxis = d3
-    .axisBottom(x)
-    .tickValues(x.domain().filter((d, i) => i % 1 === 0));
-
-  const xBarAxis = d3.axisBottom(xBars).tickValues(xBars.domain());
-
-  const yAxis = d3
-    .axisLeft(y)
-    .tickValues(y.domain().filter((d, i) => i % 5 === 0));
-
-  useEffect(() => {
-    if (
-      !svgRef.current ||
-      !xAxisRef.current ||
-      !xBarAxisRef.current ||
-      !yAxisRef.current
-    )
-      return;
-
-    const xAxisEl = d3.select(xAxisRef.current);
-    const xBarAxisEl = d3.select(xBarAxisRef.current);
-    const yAxisEl = d3.select(yAxisRef.current);
-
-    xAxisEl.call(xAxis);
-    xBarAxisEl.call(xBarAxis);
-    yAxisEl.call(yAxis);
-  }, [svgRef, xAxisRef, xBarAxisRef, yAxisRef]);
-  return (
-    <svg
-      ref={svgRef}
-      className="bg-amber-100"
-      style={{
-        width: `${width + margin.left + margin.right}`,
-        height: `${height + margin.top + margin.bottom}`,
-      }}
-    >
-      {children}
-      <g ref={yAxisRef} transform="translate(40, 0)" />
-      <g ref={xAxisRef} transform={`translate(40, ${height})`} />
-      <g ref={xBarAxisRef} transform={`translate(40, ${height})`} />
-    </svg>
-  );
-};
+import Axis from "./components/axis";
 
 const BarChart = ({ data, x, y }) => {
   const gRef = useRef(null);
